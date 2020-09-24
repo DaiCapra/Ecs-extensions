2c990cdd8dd72880bd4a7265a4853f4d6b6a84b9
DaiCapra <daniel@artmann.co>
2020-09-24 06:08:27 +0200

    first commit

---
+++ EcsExtensions.cs
@@ -0,0 +1,54 @@
+﻿using Unity.Entities;
+using UnityEngine;
+
+namespace Code.Engine.Ecs
+{
+    public static class EcsExtensions
+    {
+        public static SystemBase Get<T>(this World world)
+            where T : SystemBase
+        {
+            return world?.GetOrCreateSystem<T>();
+        }
+
+        public static void Set<T>(this EntityManager entityManager, Entity entity, T component)
+            where T : struct, IComponentData
+        {
+            if (entityManager.HasComponent<T>(entity))
+            {
+                entityManager.SetComponentData<T>(entity, component);
+            }
+            else
+            {
+                entityManager.AddComponentData<T>(entity, component);
+            }
+        }
+
+
+        public static bool TryGet<T>(this EntityManager entityManager, Entity entity, out T component)
+            where T : struct, IComponentData
+        {
+            component = default;
+            if (!entityManager.HasComponent<T>(entity))
+            {
+                return false;
+            }
+
+            component = entityManager.GetComponentData<T>(entity);
+            return true;
+        }
+
+        public static bool TryGetObject<T>(this EntityManager entityManager, Entity entity, out T component)
+            where T : Component
+        {
+            component = default;
+            if (!entityManager.HasComponent<T>(entity))
+            {
+                return false;
+            }
+
+            component = entityManager.GetComponentObject<T>(entity);
+            return true;
+        }
+    }
+}
